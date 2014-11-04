972e053a83aaa3795efbc090586c7b2d87749d27
Alexander Otavka <zotavka@gmail.com>
2014-11-03 18:07:40 -0800

    Added paging for the results of a query.

--- api/__init__.py
+++ api/__init__.py
@@ -12,9 +12,9 @@ from google.appengine.ext import ndb
 from libs.endpoints_proto_datastore.ndb import EndpointsModel
 
 
-WEB_CLIENT_ID = ""  # TODO make this secure
-ANDROID_CLIENT_ID = ""  # TODO figure out android at some point
-IOS_CLIENT_ID = ""  # probably not, unless I get some help
+WEB_CLIENT_ID = ''  # TODO make this secure
+ANDROID_CLIENT_ID = ''  # TODO figure out android at some point
+IOS_CLIENT_ID = ''  # probably not, unless I get some help
 ANDROID_AUDIENCE = WEB_CLIENT_ID
 
 
@@ -24,14 +24,14 @@ class Note(EndpointsModel):
     date_created = ndb.DateTimeProperty(auto_now_add=True)
     owner = ndb.UserProperty()
 
-'''
+"""
 class NoteMessage(messages.Message):
     title = messages.StringField(1)
     content = messages.StringField(2)
     # Hotness
     hotness = messages.IntegerField(3, default=0)
     # Color
-    color = messages.BytesField(4, default="#ffffff")
+    color = messages.BytesField(4, default='#ffffff')
     # Attach
         # Due Date
     due_date = message_types.DateTimeField(5)
@@ -39,7 +39,7 @@ class NoteMessage(messages.Message):
     reminders = message_types.DateTimeField(6, repeated=True)
         # Task : is a note
         # Note
-    sub_notes = messages.MessageField("Note", 7, repeated=True)
+    sub_notes = messages.MessageField('Note', 7, repeated=True)
         # Image : is a file
         # File
     # A URL to a file.  The file can probably be served by the datastore.
@@ -55,24 +55,24 @@ class NoteCollectionMessage(messages.Message):
 
 
 FOO_NOTES = NoteCollection(items=[
-    Note(#dsid="esgihsel",
-         title="my first note!",
-         content="this is my very first note"),
-    Note(#dsid="3f2o02hg",
-         title="my second note!",
-         content="i have more notes"),
-    Note(#dsid="0evwhfwf",
-         title="my third note!",
-         content="",
+    Note(#dsid='esgihsel',
+         title='my first note!',
+         content='this is my very first note'),
+    Note(#dsid='3f2o02hg',
+         title='my second note!',
+         content='i have more notes'),
+    Note(#dsid='0evwhfwf',
+         title='my third note!',
+         content='',
          color=None,
          hotness=2),
-    Note(#dsid="rkbn31ha",
-         title="my first note"),
+    Note(#dsid='rkbn31ha',
+         title='my first note'),
     ])
-'''
+"""
 
 
-@endpoints.api(name="remember", version="v1",
+@endpoints.api(name='helloendpoints', version='v1',
                allowed_client_ids=[WEB_CLIENT_ID, ANDROID_CLIENT_ID,
                                    endpoints.API_EXPLORER_CLIENT_ID],
                audiences=[ANDROID_AUDIENCE],
@@ -81,20 +81,21 @@ class EndpointsTestAPI(remote.Service):
     """This is a test for Endpoints, a learning experience."""
 
     #@endpoints.method(message_types.VoidMessage, NoteCollection,
-    #                  path="notes", http_method="GET",
-    #                  name="notes.list")
+    #                  path='notes', http_method='GET',
+    #                  name='notes.list')
     #def notes_list(self, request):
     #    return FOO_NOTES
 
     @Note.method(user_required=True,
-                 path="note", http_method="PUT", name="note.add")
+                 path='note', http_method='PUT', name='note.add')
     def note_add(self, note):
         note.owner = endpoints.get_current_user()
         note.put()
         return note
 
     @Note.query_method(user_required=True,
-                       path="notes", http_method="GET", name="notes.list")
+                       query_fields=('limit', 'order', 'pageToken'),
+                       path='notes', http_method='GET', name='notes.list')
     def notes_list(self, query):
         return query.filter(Note.owner == endpoints.get_current_user())
 
